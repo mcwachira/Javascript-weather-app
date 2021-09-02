f8d961b70f630b1522797744cebc9808cd4cbb6c
mcwachira <23430963+mcwachira@users.noreply.github.com>
2021-09-02 11:55:14 +0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -4,7 +4,7 @@ const key = 	"EuJoboBbS0UepIiT41tHdbDhbWgxnn0W";
 //Get weather information
 const getWeather = async(id) => {
 
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
     const query =`${id}?apikey=${key}`;
     const response = await fetch (base + query);//returns a promise
     const data = await response.json() // also returns a promise
@@ -12,7 +12,7 @@ const getWeather = async(id) => {
 }
 
 const getCity = async (city) =>{
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     const query =`?apikey=${key}&q=${city}`;
 
     const response = await fetch(base + query);
