ef0671a2b3ab93368bb6d48f213ed9afaada5b84
mcwachira <23430963+mcwachira@users.noreply.github.com>
2021-09-02 11:50:59 +0300

    initial commit

---
+++ js/api.js
@@ -0,0 +1,22 @@
+const key = 	"EuJoboBbS0UepIiT41tHdbDhbWgxnn0W";
+
+
+//Get weather information
+const getWeather = async(id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query =`${id}?apikey=${key}`;
+    const response = await fetch (base + query);//returns a promise
+    const data = await response.json() // also returns a promise
+    return data[0];
+}
+
+const getCity = async (city) =>{
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query =`?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data  = await response.json();
+    return data[0]; // returrns a promise
+}
+
---
+++ js/app.js
@@ -0,0 +1,250 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card')
+const details  = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+console.log(icon);
+const updateUi = (data) =>  {
+    // const cityDetails = data.cityDetails;
+    // const weather = data.weather;
+
+    //weather.IsDayTime
+    //destructuring
+    const {cityDetails, weather} = data;
+
+    //update the details template
+    details.innerHTML = `
+ <h5 class="my-3">${cityDetails.EnglishName}</h5>
+  <div class="my-3"> ${weather.WeatherText}</div>
+  <div class="display-4 my-4">
+           <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;c</span>
+        </div>
+    `;
+
+    //update night/day images  & weather icons
+
+    let iconSrc = `../img/icons/${weather.WeatherIcon}.svg`;
+
+icon.setAttribute('src',iconSrc);
+
+
+
+    let timeSrc =  null;
+    if(weather.IsDayTime){
+        timeSrc="../img/day.svg"
+    }else{
+        timeSrc="../img/night.svg"
+    }
+  time.setAttribute('src', timeSrc);
+
+
+    if(card.classList.contains('d-none'))   {
+        card.classList.remove('d-none');
+    }
+
+
+
+
+}
+
+const updateCity  = async( city) => {
+    const cityDetails = await getCity(city) //wait for promise to be resolved
+    const weather = await getWeather(cityDetails.Key);
+    console.log(weather)
+return{
+    cityDetails :cityDetails,
+    weather :weather
+}
+}
+
+
+cityForm.addEventListener('submit', e =>{
+    e.preventDefault();
+    //const city = input.value
+ const city =  cityForm.city.value.trim();
+ console.log(city)
+    cityForm.reset();
+
+ //update ui with new city
+    updateCity(city).then(data =>{
+        console.log(data);
+     updateUi(data);
+    }).catch(error =>{
+        console.log(error)
+    })
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
